8a417b739fbabc3627feba623685855980d7749f
kgb <kgiandepot@gmail.com>
2022-01-30 19:38:05 +0200

    add caseInsesitive input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,11 +14,11 @@ function computerPlay () {
 }
 // console.log(computerPlay()); 
 
-//e.g. playerSelection = 'Rock' 
+//e.g. playerSelection = 'Rock'  'rOcK'
 //     computerSelection = 'Scissors' 
 
-
 function playRound (playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection) {
         return "Tie! No one wins..";
     } else if ( playerSelection === "Rock" && computerSelection === "Scissors" ||
@@ -30,6 +30,18 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "Rock";
-const computerSelection = "Scissors";
+function firstOnlyCapital (inputString) {
+    const inputLowercase = inputString.toLowerCase();
+    const inputFirstPart = inputLowercase.slice(0,1).toUpperCase();
+    const inputSecondPart = inputLowercase.slice(1);
+    return inputFirstPart + inputSecondPart;
+}
+
+let playerSelection = "PaPeR";
+let computerSelection = "ScisSors";
+
+playerSelection = firstOnlyCapital(playerSelection);
+computerSelection = firstOnlyCapital(computerSelection)
+
 console.log(playRound(playerSelection, computerSelection));
+
